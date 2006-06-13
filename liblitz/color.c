3a77bee9aa3c2ca1991eef1f0ffa6be21f8de348
Kris Maglione <bsdaemon@wmii.de>
2006-06-13 18:42:20 -0400

    Added intmap.c and LICENSE from p9p at least for the short term

--- liblitz/color.c
+++ liblitz/color.c
@@ -9,25 +9,26 @@
 #include "blitz.h"
 
 static unsigned long
-xloadcolor(Display *dpy, int mon, char *colstr)
+xloadcolor(char *colstr)
 {
 	XColor color;
 	char col[8];
 
 	cext_strlcpy(col, colstr, sizeof(col));
 	col[7] = 0;
-	XAllocNamedColor(dpy, DefaultColormap(dpy, mon), col, &color, &color);
+	XAllocNamedColor(__blitz.display,
+			DefaultColormap(__blitz.display, __blitz.screen), col, &color, &color);
 	return color.pixel;
 }
 
 int
-blitz_loadcolor(Display *dpy, BlitzColor *c, int mon, char *colstr)
+blitz_loadcolor(BlitzColor *c, char *colstr)
 {
 	if(!colstr || strlen(colstr) != 23)
 		return -1;
-	c->fg = xloadcolor(dpy, mon, &colstr[0]);
-	c->bg = xloadcolor(dpy, mon, &colstr[8]);
-	c->border = xloadcolor(dpy, mon, &colstr[16]);
+	c->fg = xloadcolor(&colstr[0]);
+	c->bg = xloadcolor(&colstr[8]);
+	c->border = xloadcolor(&colstr[16]);
 	return 0;
 }
 
